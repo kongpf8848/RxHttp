79e42d243f4f1c1af8df21f587e3f45140ea50f9
孔鹏飞 <569572008@qq.com>
2019-09-29 18:05:21 +0800

    add SimpleHttpCallback.java

---
+++ RxHttp/src/main/java/com/github/kongpf8848/rxhttp/callback/SimpleHttpCallback.java
@@ -0,0 +1,23 @@
+package com.github.kongpf8848.rxhttp.callback;
+
+public class SimpleHttpCallback<T> extends HttpCallback<T> {
+    @Override
+    public void onStart() {
+
+    }
+
+    @Override
+    public void onResponse(T response) {
+
+    }
+
+    @Override
+    public void onError(Throwable e) {
+
+    }
+
+    @Override
+    public void onComplete() {
+
+    }
+}
