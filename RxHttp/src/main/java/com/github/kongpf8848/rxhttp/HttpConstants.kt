5be977c5d0c3622d461d9b41826025bf83b19e2f
kongpengfei <kongpf8848@gmail.com>
2021-02-03 17:27:37 +0800

    update README.md

--- RxHttp/src/main/java/com/github/kongpf8848/rxhttp/HttpConstants.kt
+++ RxHttp/src/main/java/com/github/kongpf8848/rxhttp/HttpConstants.kt
@@ -1,7 +1,7 @@
 package com.github.kongpf8848.rxhttp
 
 object HttpConstants {
-    const val TIME_OUT = 15L
+    const val TIME_OUT = 60L
     const val MIME_TYPE_BINARY = "application/octet-stream"
     const val MIME_TYPE_TEXT = "text/plain; charset=utf-8"
     const val MIME_TYPE_JSON = "application/json; charset=utf-8"
