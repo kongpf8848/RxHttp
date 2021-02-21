e75e5488de5ce2ed758024bdb6cdec613c9527ca
kongpengfei <kongpf8848@gmail.com>
2021-02-22 00:25:27 +0800

    优化代码

--- RxHttp/src/main/java/com/github/kongpf8848/rxhttp/bean/DownloadInfo.kt
+++ RxHttp/src/main/java/com/github/kongpf8848/rxhttp/bean/DownloadInfo.kt
@@ -1,6 +1,8 @@
 package com.github.kongpf8848.rxhttp.bean
 
-class DownloadInfo(var url: String, var dir: String, var fileName: String,var total: Long = 0, var progress: Long = 0)  {
+import java.io.Serializable
+
+class DownloadInfo(var url: String, var dir: String, var fileName: String,var total: Long = 0, var progress: Long = 0):Serializable  {
 
     override fun toString(): String {
         return "DownloadInfo(url='$url', dir='$dir', fileName='$fileName', total=$total, progress=$progress)"
