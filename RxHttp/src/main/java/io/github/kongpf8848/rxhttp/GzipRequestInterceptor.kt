a3946e02fce4f6a24213dfca67e0bfdc35553322
kongpf8848 <kongpf8848@gmail.com>
2025-03-03 13:32:37 +0800

    add GzipRequestInterceptor

---
+++ RxHttp/src/main/java/io/github/kongpf8848/rxhttp/GzipRequestInterceptor.kt
@@ -0,0 +1,41 @@
+package io.github.kongpf8848.rxhttp
+
+import okhttp3.Interceptor
+import okhttp3.MediaType
+import okhttp3.RequestBody
+import okhttp3.Response
+import okio.BufferedSink
+import okio.GzipSink
+import okio.buffer
+
+class GzipRequestInterceptor : Interceptor {
+
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val originRequest = chain.request()
+        if (originRequest.body == null || originRequest.header("Content-Encoding") != null) {
+            return chain.proceed(originRequest);
+        }
+        val newRequest = originRequest.newBuilder().header("Content-Encoding", "gzip")
+            .method(originRequest.method, gzip(originRequest.body!!))
+            .build()
+        return chain.proceed(newRequest)
+    }
+
+    private fun gzip(body: RequestBody) = object : RequestBody() {
+
+        override fun contentType(): MediaType? {
+            return body.contentType()
+        }
+
+        override fun contentLength(): Long {
+            return -1
+        }
+
+        override fun writeTo(sink: BufferedSink) {
+            val gzipSink = GzipSink(sink).buffer()
+            body.writeTo(gzipSink)
+            gzipSink.close()
+        }
+
+    }
+}
